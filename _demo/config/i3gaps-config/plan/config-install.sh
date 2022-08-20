620fa79a9886b06e83ad4d863c2b6d41fcdd31b9
samwhelp <samwhelp@users.noreply.github.com>
2022-08-21 07:31:26 +0800

    fix

---
+++ _demo/config/i3gaps-config/plan/config-install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: i3wm_config_install
+##
+i3wm_config_install () {
+
+	echo
+	echo "##"
+	echo "## Config: i3wm_config_install"
+	echo "##"
+	echo
+
+	#echo "./config/i3/bin/maintain-install.sh"
+	#./config/i3/bin/maintain-install.sh
+
+	echo "make -C config/i3 install"
+	make -C config/i3 install
+
+	echo
+
+}
+##
+### Tail: i3wm_config_install
+################################################################################
+
+
+################################################################################
+### Head: main
+##
+main_config_install () {
+	i3wm_config_install
+}
+## start
+main_config_install
+
+##
+### Tail: main
+################################################################################
